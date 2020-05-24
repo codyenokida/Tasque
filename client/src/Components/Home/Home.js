cf3c1da2928a3a0f2451e81fb9bffb64bb13857e
Cody Enokida <codyenokida@gmail.com>
2020-05-24 02:13:14 -0700

    Styling Changes

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -16,7 +16,11 @@ const useStyles = makeStyles((theme) => ({
     width: '90%',
     height: '90%',
     padding: 20,
-    borderRadius: '30px'
+    borderRadius: '30px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'left'
   }, 
   label: {
     color: '#EDB57B'
@@ -44,7 +48,7 @@ const LoginButton = withStyles(() => ({
     '&:hover': {
       backgroundColor: 'rgb(226,131,108)',
     },
-    marginRight: '20px',
+    margin: '10px 20px 10px 0',
     textTransform: 'none'
   },
 }))(Button);
@@ -54,7 +58,7 @@ const SignUpButton = withStyles(() => ({
     color: 'white',
     border: '1px solid',
     borderColor: 'white',
-    marginRight: '20px',
+    margin: '10px 20px 10px 0',
     textTransform: 'none'
   },
 }))(Button);
@@ -112,86 +116,92 @@ const Home = props => {
 
   return (
     <div className={classes.root}>
-      <Grid container>
-        <Grid 
-          xs={12} sm={6}
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justify="center"
-          style={{ minHeight: '100vh' }}>
-            <div>
-              <h1>This is <span style={{color: '#EDB57B'}}>Tasque.</span></h1>
-              <h3>Welcome back! Pleae sign in to your account.</h3>
-              <TextField
-                id="username"
-                name="username"
-                label="Username"
-                style={{ margin: "10px 0 10px 0" }}
-                fullWidth
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                  classes: {
-                    root: classes.label,
-                  },
-                }}
-                InputProps={{
-                  classes: {
-                    root: classes.inputBaseRoot,
-                    underline: classes.underline,
-                  }
-                }}
-                value={user.username}
-                onChange={handleChange}
-              />
-              <TextField
-                id="password"
-                name="password"
-                label="Password"
-                style={{ margin: "10px 0 10px 0" }}
-                fullWidth
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                  classes: {
-                    root: classes.label,
-                  },
-                }}
-                InputProps={{
-                  classes: {
-                    root: classes.inputBaseRoot,
-                    underline: classes.underline,
-                  }
-                }}
-                value={user.password}
-                onChange={handleChange}
-              />
-              <div className={classes.buttonContainer}>
-                <LoginButton 
-                  size="large"
-                  onClick={onSubmitLogin}>
-                  Login
-                </LoginButton>
-                <SignUpButton 
-                  size="large"
-                  onClick={onSubmitSignUp}>
-                  Sign Up
-                </SignUpButton>
-              </div>
+        <Grid container>
+            <Grid 
+            xs={12} sm={6}
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}>
+                <div>
+                    <h1 style={{fontSize: '3.5rem', marginBottom: '10px'}}>This is <span style={{color: '#EDB57B'}}>Tasque.</span></h1>
+                    <h3 style={{color: '#C4C4C4', fontWeight: 100}}>Welcome back! Pleae sign in to your account.</h3>
+                    <TextField
+                        id="username"
+                        name="username"
+                        label="Username"
+                        style={{ margin: "10px 0 10px 0" }}
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                            classes: {
+                                root: classes.label,
+                            },
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: classes.inputBaseRoot,
+                                underline: classes.underline,
+                            }
+                        }}
+                        value={user.username}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        id="password"
+                        name="password"
+                        label="Password"
+                        style={{ margin: "10px 0 10px 0" }}
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                            classes: {
+                                root: classes.label,
+                            },
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: classes.inputBaseRoot,
+                                underline: classes.underline,
+                            }
+                        }}
+                        value={user.password}
+                        onChange={handleChange}
+                    />
+                    <div className={classes.buttonContainer}>
+                        <LoginButton 
+                            size="large"
+                            onClick={onSubmitLogin}>
+                            Login
+                        </LoginButton>
+                        <SignUpButton 
+                            size="large"
+                            onClick={onSubmitSignUp}>
+                            Sign Up
+                        </SignUpButton>
+                    </div>
             </div>
+            </Grid>
+            <Grid 
+                xs={12} sm={6}
+                container
+                spacing={0}
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}>
+                <Paper 
+                className={classes.paper}>
+                    <div style={{padding: 50}}>
+                        <h1 style={{color: '#262B3F'}}>How does <span style={{color: '#EDB57B'}}>Tasque</span> work?</h1>
+                        <p><span style={{color: '#EDB57B'}}>Tasque</span> is a simple task manager! Start your own personalized workspace and keep track of tasks. Being by creating a category and then creating a task.</p>
+                    </div>
+                </Paper>
+            </Grid>
         </Grid>
-        <Grid 
-          xs={12} sm={6}
-          container
-          spacing={0}
-          alignItems="center"
-          justify="center"
-          style={{ minHeight: '100vh' }}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-      </Grid>
     </div>
   );
 }
